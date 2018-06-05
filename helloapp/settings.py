9a0818f205ccda2914cebfdb6cc0ac4b783e64b3
pylynx <antonoiumadalin@gmail.com>
2018-06-05 12:41:55 +0300

    Fixed contact on all pages.

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'eex-b)+ly_ynag5b_2b$8r+#cnuhtvoem07_&a(r2my4%nt4vg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
